56ac581487b1a18663faca75fb0c15357e0aef78
edilson gomes de lima <edskateicevida@gmail.com>
2023-06-17 18:59:55 -0300

    Uma nova atualização

---
+++ metodo_objetos_variaveis.py
@@ -0,0 +1,9 @@
+x = input('Digite algo')
+print('È um numero? ', x.isnumeric())
+print('È um alfanumerico (numeros e letras)? ', x.isalnum())
+print('È um alfabetico (letras)? ' , x.isalpha())
+print('Esta em minusculo? ' , x.islower())
+print('Esta em maiusculo? ' , x.isupper())
+print('È somente espaço? ' , x.isspace())
+print('Esta capitalizada (possui maiusculas e minusculas)' , x.istitle())
+
---
+++ modulos.py
@@ -0,0 +1,8 @@
+import math
+num = int(input('digite um numero: '))
+
+raizquadrada = math.sqrt(num)
+
+print('Raiz de {} e = {}' .format(num. raizquadrada)) # type: ignore
+
+print('Raiz de {} arredondada para próximo inteiro  é {}'.format(num.math.ceil(raizquadrada))) # type: ignore
